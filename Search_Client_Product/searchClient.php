972ddb231ea6ca1457a44069e19b9cf7c3c5e3b4
Keshawn Reid <32914005+KeshawnReid@users.noreply.github.com>
2021-01-17 20:15:52 -0500

    Add files via upload

---
+++ Search_Client_Product/searchClient.php
@@ -0,0 +1,17 @@
+<html>
+<head> <title>Search User</title> </head>
+
+<body>
+
+<h1>Search User Form</h1>
+
+	<form name="user" method="post" action="searchServer.php">
+	
+	Email:<input type="text" length="60" name="email" />
+	ID:<input type="number" length= "255" name="ID" />
+	Name: <input type="text" length="60" name="fName" />
+	<input type="submit" value="Search" />
+
+</form>
+</body>
+</html>
